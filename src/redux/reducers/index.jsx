b1234e13a38c2a3a458946125e84568864e646c3
kdrzclk <kadir.ozcelik16@gmail.com>
2022-05-06 10:16:35 +0300

    redux dosyaları silindi

--- src/redux/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import appReducer from "./appReducers";
-import authReducer from "./authReducer";
-
-export const rootReducer = combineReducers({
-  auth: authReducer,
-  app: appReducer,
-});
--- src/redux/types/appTypes.jsx
+++
@@ -1,2 +0,0 @@
-export const SET_LOADING = "SET_LOADING";
-export const CLEAR_LOADING = "CLEAR_LOADING";

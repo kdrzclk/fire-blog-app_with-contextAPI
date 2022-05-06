7c32b3439b73e4160e450e79bf54a2dc9ad200cf
kdrzclk <kadir.ozcelik16@gmail.com>
2022-05-06 10:52:54 +0300

    firebase functionlar hazırlandı

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,4 +1,6 @@
+import { Password } from "@mui/icons-material";
 import React, { useContext, createContext } from "react";
+import { auth, googleProvider } from "../utils/firebaseUtil";
 
 const AuthContext = createContext(); // öncelikle createcontext kullanılarak authcontext adından bir context oluştururuz (1)
 
@@ -10,9 +12,35 @@ export function useAuth() {
 }
 
 const AuthContextProvider = () => {
-  const signup = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    auth.signOut();
+  }
+
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account" });
+    auth.signInWithPopup(googleProvider);
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
   return <div></div>;
 };
 

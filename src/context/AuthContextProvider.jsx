06137fd476db4f5c93297cfc541d68ccaea1428d
kdrzclk <kadir.ozcelik16@gmail.com>
2022-05-06 10:39:20 +0300

    firebase ve env dosyası eklendi

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -2,13 +2,18 @@ import React, { useContext, createContext } from "react";
 
 const AuthContext = createContext(); // öncelikle createcontext kullanılarak authcontext adından bir context oluştururuz (1)
 
-// contextetn bilgi çekmek için, bu method herhangi bir yerden çağrıldıında useConetxt sayesinde çağırdığımız bilgileri döndürecek
-function useAuth() {
-  return useContext(AuthContext);
+// contextetn bilgi çekmek için, bu method herhangi bir yerden çağrıldıında useConetxt sayesinde çağırdığımız bilgileri döndürecek (2)
+
+//bu contexti okuyacağımız method birçok component tarafından kullanılacak buralardan bunları okyabilmek için useAuth methodunu çağırmak gerekir. bunun için export yapılması gerekir (3)
+export function useAuth() {
+  return useContext(AuthContext); //(2)
 }
 
 const AuthContextProvider = () => {
-  return <div>AuthProvider</div>;
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  return <div></div>;
 };
 
 export default AuthContextProvider;

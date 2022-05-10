c3c29179d2df337aad3326c9f7a8f77ca9b510a1
kdrzclk <kadir.ozcelik16@gmail.com>
2022-05-10 14:16:47 +0300

    updateblog tamamlandık

--- src/pages/UpdateBlog.jsx
+++ src/pages/UpdateBlog.jsx
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  Box,
+  Avatar,
+  Typography,
+  Grid,
+  CssBaseline,
+} from "@mui/material";
+import BlogForm from "../components/BlogForm";
 
-const UpdateBlog = () => {
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContextProvider";
+import { useBlog } from "../context/BlogContextProvider";
+import { toastSuccess, toastError } from "../helpers/toastNotify";
+import blog from "../assets/blok.png";
+
+const UpdateBlog = ({ match }) => {
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+  const { getOneBlog, updateBlog } = useBlog();
+  const result = getOneBlog(match.params.id);
+  const res = result ? result[0] : { title: "", content: "", image: "" };
+
+  const [newBlog, setNewBlog] = useState(res);
+
+  const handler = (newBlog) => {
+    try {
+      updateBlog(newBlog);
+      navigate("/");
+      toastSuccess("Blog Updated");
+    } catch {
+      toastError("Blog can not be updated");
+    }
+  };
+
   return (
-    <div>
-      <h1>UpdateBlog</h1>
-    </div>
+    <Container maxWidth="sm" sx={{ height: "fit-content", maxWidth: "600px" }}>
+      <CssBaseline />
+      <Box
+        sx={{
+          height: "100%",
+          marginTop: "10vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          alt="avatar_img"
+          src={newBlog?.image}
+          sx={{
+            width: 220,
+            height: 220,
+            marginTop: "20px",
+            backgroundColor: "#046582",
+          }}
+        />
+
+        <Typography variant="h4" component="h1" sx={{ m: 4, color: "#046582" }}>
+          ── Update Blog ──
+        </Typography>
+        <BlogForm blog={newBlog} handler={handler} />
+      </Box>
+    </Container>
   );
 };
 
